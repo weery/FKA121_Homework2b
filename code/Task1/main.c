82ff73b00d41d18aa9413f12593c0d2b92cec343
Weery <vicnilss@student.chalmers.se>
2016-11-22 12:58:37 +0100

    Removed errors

--- code/Task1/main.c
+++ code/Task1/main.c
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include "rng_gen.h"
+#define nbr_of_dimensions 3
 
 double trial_wave(double*, double*, int, double);
 
@@ -21,6 +22,8 @@ double local_energy(double*, double*, int, double);
 double array_mult(double*, double*, int);
 
 void new_configuration(double *, double*, int);
+
+void array_scalar(double*,double*, int, double);
 
 /* Main program */
 int main()
@@ -35,8 +38,8 @@ int main()
     double e4pi;
     double alpha;
 
-    double[3] r_1;
-    double[3] r_2;
+    double r_1[nbr_of_dimensions];
+    double r_2[nbr_of_dimensions];
 
     // Initialize Variables
     h_bar   = 1;
@@ -45,7 +48,9 @@ int main()
     e4pi    = 1;
     alpha   = 0.1;
 
-    new_configuration(r_1,r_2);
+
+
+    new_configuration(r_1,r_2, nbr_of_dimensions);
 
 
 
@@ -55,15 +60,15 @@ int main()
     return 0;
 }
 
-double trial_wave(double* r_1, double* r_2, int nbr_of_dimensions, double alpha)
+double trial_wave(double* r_1, double* r_2, int dims, double alpha)
 {
-    double r1 = array_abs(r_1,nbr_of_dimensions);
-    double r2 = array_abs(r_2,nbr_of_dimensions);
-    double[3] r_12;
-    array_diff(r_1,r_2,nbr_of_dimensions,r12);
-    double r_12 = array_abs(r_12,nbr_of_dimensions);
+    double r1 = array_abs(r_1,dims);
+    double r2 = array_abs(r_2,dims);
+    double r_12[dims];
+    array_diff(r_1,r_2,dims,r_12);
+    double r12 = array_abs(r_12,dims);
 
-    double f_val = exp(-2*r_1)*exp(-2*r_2)*exp(r12/(2*(1+alpha*r12)));
+    double f_val = exp(-2*r1)*exp(-2*r2)*exp(r12/(2*(1+alpha*r12)));
 
     return f_val;
 }
@@ -85,20 +90,22 @@ void array_diff(double* arr_1, double* arr_2, int N, double* diff)
         diff[i]=arr_1[i]-arr_2[i];
 }
 
-double local_energy(double* r_1, double* r_2, int nbr_of_dimensions, double alpha)
+double local_energy(double* r_1, double* r_2, int dims, double alpha)
 {
-    double r1 = array_abs(r_1,nbr_of_dimensions);
-    double r2 = array_abs(r_2,nbr_of_dimensions);
-    double[3] r_12;
-    array_diff(r_1,r_2,nbr_of_dimensions,r12);
-    double r_12 = array_abs(r_12,nbr_of_dimensions);
+    double r1 = array_abs(r_1,dims);
+    double r2 = array_abs(r_2,dims);
+    double r_12[dims];
+    array_diff(r_1,r_2,dims,r_12);
+    double r12 = array_abs(r_12,dims);
 
-    double* unit_1 = r_1/r1;
-    double* unit_2 = r_2/r2;
-    double[3] unit_diff;
-    array_diff(unit_1,unit_2,nbr_of_dimensions,unit_diff);
+    double* unit_1 = malloc(sizeof(double)*dims);
+    double* unit_2 = malloc(sizeof(double)*dims);
+    array_scalar(unit_1, r_1,dims,1/r1);
+    array_scalar(unit_2, r_2,dims,1/r2);
+    double unit_diff[dims] ;
+    array_diff(unit_1,unit_2,dims,unit_diff);
 
-    double term1 = array_mult(unit_diff,r_12)/(r12*(1+alpha*r12)*(1+alpha*r12));
+    double term1 = array_mult(unit_diff,r_12,dims)/(r12*(1+alpha*r12)*(1+alpha*r12));
     double term2 = - 1/(r12*(1+alpha*r12)*(1+alpha*r12)*(1+alpha*r12));
     double term3 = - 1/(4*(1+alpha*r12)*(1+alpha*r12)*(1+alpha*r12)*(1+alpha*r12));
     double term4 = 1/r12;
@@ -118,11 +125,18 @@ double array_mult(double* arr_1, double* arr_2, int N)
     return sum;
 }
 
-void new_configuration(double * r_1, double* r_2, int nbr_of_dimensions)
+void new_configuration(double * r_1, double* r_2, int dims)
 {
-    for (int i = 0; i < nbr_of_dimensions; i++)
+    for (int i = 0; i < dims; i++)
     {
         r_1[i]=randq();
         r_2[i]=randq();
     }
+}
+
+
+void array_scalar(double* arr_out, double* arr , int N , double scalar)
+{
+    for (int i = 0; i < N; i++)
+        arr_out[i]=arr[i]* scalar;
 }
