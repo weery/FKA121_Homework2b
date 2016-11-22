90f3f9fc579f10132b8a45f70edfdd7407fb02aa
Weery <vicnilss@student.chalmers.se>
2016-11-22 12:44:45 +0100

    Added array methods

--- code/Task1/main.c
+++ code/Task1/main.c
@@ -10,8 +10,17 @@
 #include <time.h>
 #include "rng_gen.h"
 
-double trial_wave(double*, double*, int);
+double trial_wave(double*, double*, int, double);
 
+double array_abs(double*, int);
+
+void array_diff(double*, double*, int, double*);
+
+double local_energy(double*, double*, int, double);
+
+double array_mult(double*, double*, int);
+
+void new_configuration(double *, double*, int);
 
 /* Main program */
 int main()
@@ -20,13 +29,24 @@ int main()
     Initialize_Generator();
 
     // Variables
-    double h_bar, e, m_e, e4pi;
+    double h_bar;
+    double e;
+    double m_e;
+    double e4pi;
+    double alpha;
+
+    double[3] r_1;
+    double[3] r_2;
 
     // Initialize Variables
     h_bar   = 1;
     e       = 1;
     m_e     = 1;
     e4pi    = 1;
+    alpha   = 0.1;
+
+    new_configuration(r_1,r_2);
+
 
 
 
@@ -35,7 +55,74 @@ int main()
     return 0;
 }
 
-double trial_wave(double* r_1, double* r_2, int nbr_of_particles)
+double trial_wave(double* r_1, double* r_2, int nbr_of_dimensions, double alpha)
 {
-    return 0;
+    double r1 = array_abs(r_1,nbr_of_dimensions);
+    double r2 = array_abs(r_2,nbr_of_dimensions);
+    double[3] r_12;
+    array_diff(r_1,r_2,nbr_of_dimensions,r12);
+    double r_12 = array_abs(r_12,nbr_of_dimensions);
+
+    double f_val = exp(-2*r_1)*exp(-2*r_2)*exp(r12/(2*(1+alpha*r12)));
+
+    return f_val;
+}
+
+double array_abs(double* vals, int N)
+{
+    double sum  = 0;
+    for (int i = 0; i < N; i++)
+    {
+        sum += vals[i]*vals[i];
+    }
+    sum = sqrt(sum);
+    return sum;
+}
+
+void array_diff(double* arr_1, double* arr_2, int N, double* diff)
+{
+    for (int i = 0; i < N; i++)
+        diff[i]=arr_1[i]-arr_2[i];
+}
+
+double local_energy(double* r_1, double* r_2, int nbr_of_dimensions, double alpha)
+{
+    double r1 = array_abs(r_1,nbr_of_dimensions);
+    double r2 = array_abs(r_2,nbr_of_dimensions);
+    double[3] r_12;
+    array_diff(r_1,r_2,nbr_of_dimensions,r12);
+    double r_12 = array_abs(r_12,nbr_of_dimensions);
+
+    double* unit_1 = r_1/r1;
+    double* unit_2 = r_2/r2;
+    double[3] unit_diff;
+    array_diff(unit_1,unit_2,nbr_of_dimensions,unit_diff);
+
+    double term1 = array_mult(unit_diff,r_12)/(r12*(1+alpha*r12)*(1+alpha*r12));
+    double term2 = - 1/(r12*(1+alpha*r12)*(1+alpha*r12)*(1+alpha*r12));
+    double term3 = - 1/(4*(1+alpha*r12)*(1+alpha*r12)*(1+alpha*r12)*(1+alpha*r12));
+    double term4 = 1/r12;
+
+    double energy = 0;
+    energy = -4 +term1+term2+term3+term4;
+
+    return energy;
+}
+
+
+double array_mult(double* arr_1, double* arr_2, int N)
+{
+    double sum = 0;
+    for (int i = 0; i < N ; i++)
+        sum += arr_1[i]*arr_2[i];
+    return sum;
+}
+
+void new_configuration(double * r_1, double* r_2, int nbr_of_dimensions)
+{
+    for (int i = 0; i < nbr_of_dimensions; i++)
+    {
+        r_1[i]=randq();
+        r_2[i]=randq();
+    }
 }
