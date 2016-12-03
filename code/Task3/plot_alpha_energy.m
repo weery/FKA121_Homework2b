ff073b6faf551a8a0be4b367cb15a938db1ae2f2
Weery <vicnilss@student.chalmers.se>
2016-12-03 17:03:47 +0100

    Added text to all tasks, basically only task 4 needs more work

--- code/Task3/plot_alpha_energy.m
+++ code/Task3/plot_alpha_energy.m
@@ -3,10 +3,35 @@ clear all, close all, clc
 alpha_energy=importdata('alpha_avg_energy.dat');
 
 mean_alpha = mean(alpha_energy(:,2:end),2);
+var_alpha = var(alpha_energy(:,2:end),0,2);
 
 plot(alpha_energy(:,1),mean_alpha)
 
+% not necessarily right
+errorbar(alpha_energy(:,1),mean_alpha,var_alpha*100)
+
+hold on
+[min_alpha, min_alpha_idx]=min(mean_alpha);
+plot(alpha_energy(min_alpha_idx,1),mean_alpha(min_alpha_idx),'rx')
+
+min_energy_plot=-2.879;
+
+plot([alpha_energy(min_alpha_idx,1) alpha_energy(min_alpha_idx,1)],[min_energy_plot,mean_alpha(min_alpha_idx)],'k--')
+plot([alpha_energy(1,1) alpha_energy(min_alpha_idx,1)],[mean_alpha(min_alpha_idx),mean_alpha(min_alpha_idx)],'k--')
+
+alpha_txt = sprintf('%f', alpha_energy(min_alpha_idx,1));
+alpha_txt = strcat('$\leftarrow \alpha = ',alpha_txt, '$');
+
+text(alpha_energy(min_alpha_idx,1)+0.001,mean_alpha(min_alpha_idx)+(min_energy_plot-mean_alpha(min_alpha_idx))/2,alpha_txt,'interpreter','latex')
+
+energy_txt = sprintf('%f', mean_alpha(min_alpha_idx));
+energy_txt = strcat('$\uparrow E = ',energy_txt, '$');
+
+text((alpha_energy(min_alpha_idx,1)+alpha_energy(1,1))/2-0.03,mean_alpha(min_alpha_idx)-0.0002,energy_txt,'interpreter','latex')
+
+xlim([alpha_energy(1,1),alpha_energy(end,1)])
+
 ylabel('Energy [$a.u.$]','interpreter','latex')
 xlabel('$\alpha$ [$\#$]','interpreter','latex')
-title('Estimation of lowest energy based on $\alpha$, jag behöver error bars','interpreter','latex')
+title('Estimation of lowest energy based on independent $\alpha$ runs','interpreter','latex')
 
--- code/Task3/plot_alpha_means.m
+++ code/Task3/plot_alpha_means.m
@@ -2,10 +2,36 @@ clear all, clc, clf, close all
 
 alpha_mean_var=importdata('alpha_mean_var.dat');
 
-plot(alpha_mean_var(:,1),alpha_mean_var(:,2))
+%plot(alpha_mean_var(:,1),alpha_mean_var(:,2))
 hold on
-errorbar(alpha_mean_var(:,1),alpha_mean_var(:,2),alpha_mean_var(:,3)*sqrt(11/100000))
+errorbar(alpha_mean_var(:,1),alpha_mean_var(:,2),alpha_mean_var(:,3)*sqrt(11/1000000))
+
+xlim([alpha_mean_var(1,1),alpha_mean_var(end,1)])
 
 ylabel('Energy [$a.u.$]','interpreter','latex')
 xlabel('$\alpha$ [$\#$]','interpreter','latex')
-title('Estimation of lowest energy based on $\alpha$','interpreter','latex')
+title('Estimation of lowest energy based on one $\alpha$ run','interpreter','latex')
+
+
+
+
+[min_alpha, min_alpha_idx]=min(alpha_mean_var(:,2));
+plot(alpha_mean_var(min_alpha_idx,1),alpha_mean_var(min_alpha_idx,2),'rx')
+
+min_energy_plot=-2.879;
+
+plot([alpha_mean_var(min_alpha_idx,1) alpha_mean_var(min_alpha_idx,1)],[min_energy_plot,alpha_mean_var(min_alpha_idx,2)],'k--')
+plot([alpha_mean_var(1,1) alpha_mean_var(min_alpha_idx,1)],[alpha_mean_var(min_alpha_idx,2),alpha_mean_var(min_alpha_idx,2)],'k--')
+
+alpha_txt = sprintf('%f', alpha_mean_var(min_alpha_idx,1));
+alpha_txt = strcat('$\leftarrow \alpha = ',alpha_txt, '$');
+
+text(alpha_mean_var(min_alpha_idx,1)+0.001,alpha_mean_var(min_alpha_idx,2)+(min_energy_plot-alpha_mean_var(min_alpha_idx,2))/2,alpha_txt,'interpreter','latex')
+
+energy_txt = sprintf('%f', alpha_mean_var(min_alpha_idx,2));
+energy_txt = strcat('$\uparrow E = ',energy_txt, '$');
+
+text((alpha_mean_var(min_alpha_idx,1)+alpha_mean_var(1,1))/2-0.03,alpha_mean_var(min_alpha_idx,2)-0.00025,energy_txt,'interpreter','latex')
+
+xlim([alpha_mean_var(1,1),alpha_mean_var(end,1)])
+
